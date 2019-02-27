84daff5f6f1841050ef70bc8b0dbf3b078b36d11
Gustav Schmid <gustav.schmid@gustavs-mbp.speedport_w_724v_09011603_05_017>
2019-02-27 23:50:07 +0100

    server will now no longer run on localhost

--- src/main/java/server/MainServer.java
+++ src/main/java/server/MainServer.java
@@ -7,13 +7,20 @@ import server.counter.mongodb.MongodShutdown;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collections;
 
 @SpringBootApplication
 public class MainServer {
 
     public static void main(String args[]) {
         startMongoDB();
-        SpringApplication.run(MainServer.class, args);
+        String hostAdress = getIPAddress();
+        SpringApplication app = new SpringApplication(MainServer.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.address", hostAdress));
+        app.run(args);
     }
 
 
@@ -27,5 +34,16 @@ public class MainServer {
             System.out.println("unable to start mongod");
         }
         Runtime.getRuntime().addShutdownHook(new Thread(new MongodShutdown()));
+    }
+
+    public static String getIPAddress() {
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            System.out.println("IP Address:- " + inetAddress.getHostAddress());
+            return inetAddress.getHostAddress();
+        } catch (UnknownHostException e ) {
+            System.out.println("could not get host address will start on localhost instead");
+            return "127.0.0.1";
+        }
     }
 }
